3040458fb1ce0742e3dddc5fa321694bd0b723c2
Evan Lee <feuxfollets1228@gmail.com>
2025-03-02 20:18:02 -0500

    Chapter 2

--- src/Chapter_2.cpp
+++ src/Chapter_2.cpp
@@ -6,12 +6,25 @@ int P_58() {
 	return *reinterpret_cast<char*>(&x);
 }
 
-
+int P_59(int x, int y) {
+	return (x & 0xFF) | (y & ~0xFF);
+}
 
 unsigned P_60(unsigned x, int i, unsigned char b) {
 	auto byte_ptr = reinterpret_cast<unsigned char*>(&x);
 	byte_ptr[i] = b;
 	return x;
+}
+
+void P_61(int x) {
+	int A = !!x;
+	int B = !!~x;
+	int C = !!(x & 0xFF);
+	int D = !!(~x & (0xFF << (sizeof(int) - 1 << 3)));
+}
+
+int P_62() {
+	return (-1 >> 1) < 0;
 }
 
 } // namespace
